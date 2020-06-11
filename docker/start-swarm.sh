078789e3f42ed31b4f4cca5947e2a42d871309cb
Jorge Redondo Flames <jorge.redondo@vascarsolutions.com>
2020-06-11 17:27:18 -0400

    Initial commit

---
+++ docker/start-swarm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+function startMachine {
+    docker-machine stop $1
+    docker-machine start $1
+    docker-machine ssh $1 sudo cp $CERT /usr/local/share/ca-certificates/
+    docker-machine ssh $1 sudo update-ca-certificates
+}
+
+CERT=/hosthome/jredondo/Projects/data_monitor/docker/certs/domain.crt
+
+startMachine manager1 
+startMachine worker0
+startMachine worker1
+
+
+
+    
