20c132253388896adc793470a0f74cf808c82ded
Jorge Redondo Flames <jorge.redondo@vascarsolutions.com>
2020-06-17 13:33:23 -0400

    Replaced MultiplexerConsumer with IntegrityVerifierConsumer

---
+++ consumer/src/main/java/org/streamexperiments/consumer/logic/IntegrityVerifierConsumer.java
@@ -0,0 +1,91 @@
+package org.streamexperiments.consumer.logic;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.streamexperiments.models.Update;
+
+import java.util.*;
+
+/**
+ * Still very simple {@link Consumer} implementation.
+ * Keep an index by sender of message arrived.
+ *
+ * @author Jorge Redondo Flames <jorge.redondo@gmail.com>
+ *
+ */
+public class IntegrityVerifierConsumer implements Consumer {
+    private final long THRESHOLD = 10000;
+
+    private static Logger logger = LogManager.getLogger(IntegrityVerifierConsumer.class);
+
+    private Map<String, ProducerRecord> map = new HashMap<>();
+
+    public void consume(Update update) {
+        ProducerRecord record = map.computeIfAbsent(update.getSender(),
+                (key) -> new ProducerRecord(System.currentTimeMillis()));
+
+        record.update(update);
+
+        if(record.ids.size() % THRESHOLD == 0) {
+            senderLogReport(THRESHOLD, update.getSender(), record);
+            if(dataLoss(update.getSender())) {
+                printDataLossBanner();
+            }
+        }
+
+    }
+
+    public boolean dataLoss(String sender) {
+        ProducerRecord record = map.get(sender);
+        return record != null && record.dataLoss();
+    }
+
+    private void printDataLossBanner() {
+        logger.info("###############################");
+        logger.info("###############################");
+        logger.info("########## DATA LOSS ##########");
+        logger.info("###############################");
+        logger.info("###############################");
+    }
+
+    private void senderLogReport(long numberOfItems, String sender, ProducerRecord record) {
+        logger.info("SENDER: " + sender);
+        logger.info(record.ids.size() + " items.");
+        logger.info(numberOfItems + " items in: " + (System.currentTimeMillis() - record.lastTime) + "ms");
+        logger.info("Registered producers: " + map.size() + "\n\n");
+    }
+
+    private class ProducerRecord {
+        Long lastTime;
+        Set<Long> ids = new HashSet<>();
+
+        public ProducerRecord(Long time) {
+            this.lastTime = time;
+        }
+
+        public void update(Update update) {
+            ids.add(update.getId());
+            lastTime = System.currentTimeMillis();
+        }
+
+        private boolean dataLoss() {
+            List<Long> list = new ArrayList<>(this.ids);
+            Collections.sort(list);
+            long aux = -1;
+            for (Long item : list) {
+                if (aux == -1) {
+                    aux = item;
+                    continue;
+                } else {
+                    if (item - aux != 1) {
+                        return true;
+                    }
+                    aux = item;
+                }
+            }
+            return false;
+        }
+
+    }
+}
