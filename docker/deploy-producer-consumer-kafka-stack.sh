0620107d32c619d55015383353e0d5cd98e8c8bf
Jorge Redondo Flames <jorge.redondo@vascarsolutions.com>
2020-06-20 16:30:16 -0400

    Fixed kafka stack to allow doker service scale=N

---
+++ docker/deploy-producer-consumer-kafka-stack.sh
@@ -0,0 +1,2 @@
+COMPOSE_FILE=/hosthome/jredondo/Projects/flink-cep-spring-integration/docker/docker-producer-consumer-kafka-stack.yml
+docker-machine ssh manager1 REGISTRY_IP=192.168.99.147 MANAGER_IP=192.168.99.147 docker stack deploy -c $COMPOSE_FILE producer_consumer
